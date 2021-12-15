8ff32bb8a92bf8aad8b074f9e2b1bc45c93a7ef4
Mathijs van Velde <mathijs.v.velde@gmail.com>
2021-12-15 08:27:07 +0100

    Updated the script a bit

--- preinstaller.sh
+++ preinstaller.sh
@@ -10,6 +10,9 @@ apt-get update
 apt-get --yes --no-install-recommends install git openssh-client
 
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
-cat ~/.ssh/id_rsa
-nano ~/.ssh/authorized_keys
+cat ~/.ssh/id_rsa.pub
+touch ~/.ssh/authorized_keys
 touch ~/.ssh/known_hosts
+
+cp -rf ~/.ssh /home/pi/.ssh
+chown -R pi:pi /home/pi/
